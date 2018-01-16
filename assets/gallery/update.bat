1b98ae99e395ccdcf520d9f4b68325bc2b3c0dc3
DeweyMao <664151356@qq.com>
2018-01-16 11:09:06 +0800

    modify gallery

--- assets/gallery/update.bat
+++ assets/gallery/update.bat
@@ -2,38 +2,45 @@
 setlocal enabledelayedexpansion
 
 set f=../../gallery.md
+set author=Jia
 
 echo --- > !f!
 echo layout: page >> !f!
 echo title: Gallery >> !f!
 echo permalink: /gallery/ >> !f!
 echo --- >> !f!
-
 echo. >> !f!
 
 echo ^<div style="float:right"^> >> !f!
 echo  ^<a href="{{site.url}}/about/"^> ^<img style="border:none;" src="{{ site.url }}/assets/pics/reward.png" alt="Reward me"/^> ^</a^>  >> !f!
 echo ^</div^> >> !f!
-echo ### Welcome to Miss Jia's Online Gallery! >> !f!
-
+echo ### Welcome to Miss Jia's Online Gallery. >> !f!
 echo. >> !f!
 
 echo ^<div^> >> !f!
 echo  ^<table border = "1"^> >> !f!
 echo. >> !f!
 
-for %%s in (*) do ( 
- if not "%%s"=="update.bat" (
+for %%s in (*.jpg) do ( 
+  set d=%%s
+  set year=!d:~0,4!
+  set month=!d:~5,2!
+  set day=!d:~8,2!
+  set title=!d:~11,-4!
+ 
   echo  ^<tr^> >> !f!
-  echo   ^<td align="center"^> ^<b^>orchid^</b^> ^<br^> ^<span class="post-meta"^> 01-12-2018, Jia ^</span^> ^</td^> >> !f!
+  echo   ^<td align="center"^> ^<b^>!title!^</b^> ^<br^> ^<span class="post-meta"^> !month!-!day!-!year!, !author! ^</span^> ^</td^> >> !f!
   echo  ^</tr^> >> !f!
   echo  ^<tr^> >> !f!
-  echo   ^<td align="center"^> ^<img src="{{ site.url }}/assets/gallery/%%s" alt="orchid"/^> ^</td^> >> !f!
+  echo   ^<td align="center"^> ^<img src="{{ site.url }}/assets/gallery/%%s" alt="!title!"/^> ^</td^> >> !f!
   echo  ^</tr^> >> !f!
   echo. >> !f!
- )
 ) 
 
 echo. >> !f!
 echo  ^</table^> >> !f!
 echo ^</div^> >> !f!
+
+rem echo. >> !f! : blank line
+rem echo ^<div^> : ^ escaped character
+rem  set year=!d:~0,4! : extract 4 characters from the first character
--- gallery.md
+++ gallery.md
@@ -7,13 +7,13 @@ permalink: /gallery/
 <div style="float:right"> 
  <a href="{{site.url}}/about/"> <img style="border:none;" src="{{ site.url }}/assets/pics/reward.png" alt="Reward me"/> </a>  
 </div> 
-### Welcome to Miss Jia's Online Gallery 
+### Welcome to Miss Jia's Online Gallery. 
  
 <div> 
  <table border = "1"> 
  
  <tr> 
-  <td align="center"> <b>orchid</b> <br> <span class="post-meta"> 01-12-2018, Jia </span> </td> 
+  <td align="center"> <b>orchid</b> <br> <span class="post-meta"> 01-15-2018, Jia </span> </td> 
  </tr> 
  <tr> 
   <td align="center"> <img src="{{ site.url }}/assets/gallery/2018_01_15_orchid.jpg" alt="orchid"/> </td> 
